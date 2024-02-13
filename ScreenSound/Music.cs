74f1f98cfccf697fb6334ad77aa11e5d2c7475d0
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-02-13 18:24:05 -0300

    feat: creating music class

--- ScreenSound/Music.cs
+++ ScreenSound/Music.cs
@@ -1,7 +1,24 @@
 ﻿class Music
 {
-    string name;
-    string artist;
-    int duration;
-    bool available;
-}
+    public string name;
+    public string artist;
+    public double duration;
+    public bool available;
+
+    public Music(string name, string artist, double duration, bool available)
+    {
+        this.name = name;
+        this.artist = artist;
+        this.duration = duration;
+        this.available = available;
+    }
+
+    public void ShowMusicInfo()
+    {
+        Console.WriteLine($"Music's Name: {this.name}");
+        Console.WriteLine($"Music's Artist: {this.artist}");
+        Console.WriteLine($"Music's Duration: {this.duration}");
+        if (available) Console.WriteLine($"Music's Available: Disponível");
+        else Console.WriteLine($"Music's Available: Não está disponível");
+    }
+} 
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,1 +1,2 @@
-﻿
+﻿Music music = new("When I Was Your Man", "Bruno Mars", 2.36, true);
+music.ShowMusicInfo();
